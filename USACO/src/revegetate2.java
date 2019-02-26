7c0d92381840f69ad09fad9291c3875bdac16cf0
Kevin Xu <kevinxu8099@gmail.com>
2019-02-25 23:04:59 -0800

    Feb Bronze Probs for Practice

--- USACO/src/revegetate2.java
+++ USACO/src/revegetate2.java
@@ -1,8 +1,80 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class revegetate2 {
     public static void main(String[] args) throws IOException {
-        BufferedReader f = new BufferedReader(new FileReader("revegetate2.txt"));
-        //PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("revegetate.out")));
+        BufferedReader f = new BufferedReader(new FileReader("revegetate.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("revegetate.out")));
+        StringTokenizer st = new StringTokenizer(f.readLine());
+        int fields = Integer.parseInt(st.nextToken());
+        int cows = Integer.parseInt(st.nextToken());
+        int[] fieldTrack = new int[fields];
+        fieldTrack[0]=1;
+        int[][] dataHold = new int[cows][2];
+        for(int i=0; i<cows; i++){
+            StringTokenizer st2 = new StringTokenizer(f.readLine());
+            dataHold[i][0] = Integer.parseInt(st2.nextToken());
+            dataHold[i][1] = Integer.parseInt(st2.nextToken());
+        }
+        int[][] typeTrack = new int[cows][2];
+        //System.out.println(Arrays.deepToString(dataHold));
+        ArrayList<ArrayList<Integer>> type= new ArrayList<ArrayList<Integer>>();
+
+       for(int i=0; i<fields; i++){
+           type.add(new ArrayList<>());
+       }
+        for(int i=0; i<cows;i++){
+            for(int j=0; j<2;j++){
+                type.get(dataHold[i][j]-1).add(i);
+
+            }
+        }
+        //for(ArrayList<Integer> bob : type){
+        //    System.out.println(bob);
+        //}
+        for(int i : type.get(0)){
+            typeTrack[i][0]=1;
+        }
+        int[] answer = new int[fields];
+        answer[0]=1;
+        //System.out.println(Arrays.deepToString(typeTrack));
+        for(int i=1; i<fieldTrack.length;i++){
+                boolean isGo=false;
+                for(int c=1; c<=4;c++) {
+                    if(!isGo){
+                        if(checkingValid(typeTrack,type,c,i)){
+                            answer[i]=c;
+                            isGo=true;
+                        }
+                    }
+
+
+                }
+        }
+        //System.out.println(Arrays.toString(answer));
+        for(int i : answer){
+            out.print(i);
+        }
+        out.println();
+        f.close();
+        out.close();
+    }
+    public static boolean checkingValid(int[][]typeTrack, ArrayList<ArrayList<Integer>> type, int typeSeed, int field){
+        for( int b : type.get(field)){
+            if(typeTrack[b][1]==typeSeed||typeTrack[b][0]==typeSeed){
+                return false;
+            }
+        }
+        for(int b: type.get(field)){
+            if(typeTrack[b][0]==0){
+                typeTrack[b][0]=typeSeed;
+            }else{
+                if(typeTrack[b][1]==0){
+                    typeTrack[b][1]=typeSeed;
+                }
+            }
+        }
+        return true;
     }
 }
---
+++ USACO/src/traffic.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class traffic {
+    public static void main(String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader("traffic.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("traffic.out")));
+        int segments = Integer.parseInt(f.readLine());
+        int[][] data = new int[segments][3];
+        for(int i =0; i<segments;i++){
+            StringTokenizer st = new StringTokenizer(f.readLine());
+            String status = st.nextToken();
+            if(status.equals("none")){
+                data[i][0]=0;
+            }else if(status.equals("on")){
+                data[i][0]=1;
+            }else{
+                data[i][0]=2;
+            }
+            data[i][1]= Integer.parseInt(st.nextToken());
+            data[i][2] = Integer.parseInt(st.nextToken());
+        }
+        //System.out.println(Arrays.deepToString(data));
+        int firstNone=0;
+        boolean isDone = false;
+        for(int i=0; i<data.length;i++){
+            if(data[i][0]==0&&!isDone){
+                isDone=true;
+                firstNone=i;
+            }
+        }
+        int lastNone=0;
+        boolean isDone2 = false;
+        for(int i=data.length-1; i>=0;i--){
+            if(data[i][0]==0&&!isDone2){
+                isDone2=true;
+                lastNone=i;
+            }
+        }
+        ArrayList<Integer> chainNoneStart= new ArrayList<>();
+        ArrayList<Integer> chainNoneEnd = new ArrayList<>();
+
+        //System.out.println(firstNone);
+        //System.out.println(lastNone);
+        boolean isDone3 = false;
+        chainNoneStart.add(firstNone);
+        int counter=1;
+        while(!isDone3){
+            if(data[firstNone+counter][0]!=0){
+                isDone3=true;
+            }
+            if(!isDone3){
+                chainNoneStart.add(counter+firstNone);
+                counter++;
+            }
+        }
+        boolean isDone4 = false;
+        chainNoneEnd.add(lastNone);
+        int counter2=1;
+        while(!isDone4){
+            if(data[lastNone-counter2][0]!=0){
+                isDone4=true;
+            }
+            if(!isDone4){
+                chainNoneEnd.add(lastNone-counter2);
+                counter2++;
+            }
+        }
+        //System.out.println(chainNoneStart);
+        //System.out.println(chainNoneEnd);
+        int[][] currentRange = new int[2][2];
+        int maxStartRange =-1;
+        int minStartRange=1001;
+        for(int i: chainNoneStart){
+            if(data[i][1]>maxStartRange){
+                maxStartRange=data[i][1];
+            }
+            if(data[i][2]<minStartRange){
+                minStartRange=data[i][2];
+            }
+        }
+        int maxEndRange=-1;
+        int minEndRange=1001;
+        for(int i: chainNoneEnd){
+            if(data[i][1]>maxEndRange){
+                maxEndRange=data[i][1];
+            }
+            if(data[i][2]<minEndRange){
+                minEndRange=data[i][2];
+            }
+        }
+        //System.out.println(maxStartRange+" - " +minStartRange );
+        //System.out.println(maxEndRange+" - "+minEndRange);
+
+    for(int i=firstNone-1;i>=0;i--){
+        if(data[i][0]==1){
+            maxStartRange-=Math.max(data[i][1],data[i][2]);
+            minStartRange-=Math.min(data[i][1],data[i][2]);
+        }else{
+            maxStartRange+=Math.min(data[i][1],data[i][2]);
+            minStartRange+=Math.max(data[i][1],data[i][2]);
+
+        }
+    }
+    for(int i=lastNone+1;i<data.length;i++){
+        if(data[i][0]==1){
+            maxEndRange+=Math.min(data[i][1],data[i][2]);
+            minEndRange+=Math.max(data[i][1],data[i][2]);
+        }else{
+            maxEndRange-=Math.max(data[i][1],data[i][2]);
+            minEndRange-=Math.min(data[i][1],data[i][2]);
+        }
+    }
+        out.println(maxStartRange+" " +minStartRange );
+        out.println(maxEndRange+" "+minEndRange);
+        f.close();
+        out.close();
+    }
+
+}
