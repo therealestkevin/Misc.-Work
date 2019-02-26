2d51facedd17b2eb837da85d630c58689ed3716c
Kevin Xu <kevinxu8099@gmail.com>
2019-02-26 00:00:11 -0800

    updates to Traffic

--- USACO/src/traffic.java
+++ USACO/src/traffic.java
@@ -1,11 +1,12 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class traffic {
     public static void main(String[] args) throws IOException {
-        BufferedReader f = new BufferedReader(new FileReader("traffic.in"));
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("traffic.out")));
+        BufferedReader f = new BufferedReader(new FileReader("traffic.txt"));
+        //PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("traffic.out")));
         int segments = Integer.parseInt(f.readLine());
         int[][] data = new int[segments][3];
         for(int i =0; i<segments;i++){
@@ -21,7 +22,7 @@ public class traffic {
             data[i][1]= Integer.parseInt(st.nextToken());
             data[i][2] = Integer.parseInt(st.nextToken());
         }
-        //System.out.println(Arrays.deepToString(data));
+        System.out.println(Arrays.deepToString(data));
         int firstNone=0;
         boolean isDone = false;
         for(int i=0; i<data.length;i++){
@@ -41,34 +42,42 @@ public class traffic {
         ArrayList<Integer> chainNoneStart= new ArrayList<>();
         ArrayList<Integer> chainNoneEnd = new ArrayList<>();
 
-        //System.out.println(firstNone);
-        //System.out.println(lastNone);
+        System.out.println(firstNone);
+        System.out.println(lastNone);
         boolean isDone3 = false;
         chainNoneStart.add(firstNone);
         int counter=1;
         while(!isDone3){
-            if(data[firstNone+counter][0]!=0){
+            if(firstNone+counter<data.length) {
+                if (data[firstNone + counter][0] != 0) {
+                    isDone3 = true;
+                }
+                if (!isDone3) {
+                    chainNoneStart.add(counter + firstNone);
+                    counter++;
+                }
+            }else{
                 isDone3=true;
-            }
-            if(!isDone3){
-                chainNoneStart.add(counter+firstNone);
-                counter++;
             }
         }
         boolean isDone4 = false;
         chainNoneEnd.add(lastNone);
         int counter2=1;
         while(!isDone4){
-            if(data[lastNone-counter2][0]!=0){
+            if(lastNone-counter2>=0) {
+                if (data[lastNone - counter2][0] != 0) {
+                    isDone4 = true;
+                }
+                if (!isDone4) {
+                    chainNoneEnd.add(lastNone - counter2);
+                    counter2++;
+                }
+            }else{
                 isDone4=true;
             }
-            if(!isDone4){
-                chainNoneEnd.add(lastNone-counter2);
-                counter2++;
-            }
         }
-        //System.out.println(chainNoneStart);
-        //System.out.println(chainNoneEnd);
+        System.out.println(chainNoneStart);
+        System.out.println(chainNoneEnd);
         int[][] currentRange = new int[2][2];
         int maxStartRange =-1;
         int minStartRange=1001;
@@ -90,8 +99,8 @@ public class traffic {
                 minEndRange=data[i][2];
             }
         }
-        //System.out.println(maxStartRange+" - " +minStartRange );
-        //System.out.println(maxEndRange+" - "+minEndRange);
+        System.out.println(maxStartRange+" - " +minStartRange );
+        System.out.println(maxEndRange+" - "+minEndRange);
 
     for(int i=firstNone-1;i>=0;i--){
         if(data[i][0]==1){
@@ -112,10 +121,10 @@ public class traffic {
             minEndRange-=Math.min(data[i][1],data[i][2]);
         }
     }
-        out.println(maxStartRange+" " +minStartRange );
-        out.println(maxEndRange+" "+minEndRange);
+        System.out.println(maxStartRange+" " +minStartRange );
+       System.out.println(maxEndRange+" "+minEndRange);
         f.close();
-        out.close();
+        //out.close();
     }
 
 }
